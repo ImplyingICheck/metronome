bc908f8eb0d3092eddbf0a427c4e72704da5f3f5
Andrew <amhector@stanford.edu>
2024-02-29 05:41:53 -0800

    feat(): Add main gui

---
+++ src/metronome_gui/main.py
@@ -0,0 +1,8 @@
+"""The top level logic of the metronome app."""
+import tkinter
+from metronome_gui import counter
+
+if __name__ == "__main__":
+  metronome = tkinter.Tk()
+  counter_widget = counter.CounterWidget(metronome)
+  metronome.mainloop()
