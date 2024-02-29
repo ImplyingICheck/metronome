68502b8a6e0ffb07c92fcf5e805ccb3381e27c7c
Andrew <amhector@stanford.edu>
2024-02-29 05:05:12 -0800

    feat(): Change type of TypeAlias NoteValue
    Change NoteValue to be of type float or int. decimal.Decimal leads to propagating casting issues.

--- src/metronome/notes.py
+++ src/metronome/notes.py
@@ -1,12 +1,11 @@
 """Note helper class. Provides note classes with values and NoteSequence, a
 class to organize note groups."""
-import decimal
 from typing import TypeAlias
 from collections.abc import Iterable
 
 
 TimeSignature: TypeAlias = tuple[int, int]
-NoteValue: TypeAlias = decimal.Decimal
+NoteValue: TypeAlias = float | int
 
 
 class Note:
@@ -18,13 +17,13 @@ class Note:
 class WholeNote(Note):
 
   def __init__(self):
-    super().__init__(decimal.Decimal(4))
+    super().__init__(4)
 
 
 class QuarterNote(Note):
 
   def __init__(self):
-    super().__init__(decimal.Decimal(1))
+    super().__init__(1)
 
 
 class NoteSequence:
