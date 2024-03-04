ee21707a1c4141e78aa85ab4963537aac5544e5d
Andrew <amhector@stanford.edu>
2024-03-04 10:43:34 -0800

    feat(PlayButton): Implement toggle_state
    Switches button text from on to off

--- src/metronome_gui/play_button.py
+++ src/metronome_gui/play_button.py
@@ -9,11 +9,21 @@ class State(Enum):
 
 
 class PlayButton:
+  """View for displaying playback status of the metronome."""
 
   def __init__(self, master: tk.Tk):
     self._state = State.PAUSE
-    self.button = tk.Button(master)
+    self.button = tk.Button(master, command=self.toggle_state)
+    self.button.pack()
 
   @property
   def state(self) -> State:
     return self._state
+
+  def toggle_state(self):
+    if self._state == State.PAUSE:
+      self._state = State.PLAY
+      self.button.config(text="PLAY")
+    elif self._state == State.PLAY:
+      self._state = State.PAUSE
+      self.button.config(text="PAUSE")
