eb8473abe290221eec031a87a435d64bc9d7af61
Andrew <amhector@stanford.edu>
2024-03-04 11:05:39 -0800

    feat(main): Add PlayButton to main

--- src/metronome_gui/main.py
+++ src/metronome_gui/main.py
@@ -1,8 +1,11 @@
 """The top level logic of the metronome app."""
 import tkinter
 from metronome_gui import counter
+from metronome_gui import play_button
+
 
 if __name__ == "__main__":
   metronome = tkinter.Tk()
   counter_widget = counter.BPMWidget(metronome)
+  play_button_widget = play_button.PlayButton(metronome)
   metronome.mainloop()
