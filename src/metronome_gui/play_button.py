4841e9e24ea70bad4449428c0a0549127770d8e4
Andrew <amhector@stanford.edu>
2024-03-04 10:24:26 -0800

    feat(): Implement PlayButton class

--- src/metronome_gui/play_button.py
+++ src/metronome_gui/play_button.py
@@ -1,7 +1,15 @@
 """Widget displaying status of playback."""
+import tkinter as tk
 from enum import Enum
 
 
 class State(Enum):
   PAUSE = 1
   PLAY = 2
+
+
+class PlayButton:
+
+  def __init__(self, master: tk.Tk):
+    self._state = State.PAUSE
+    self.button = tk.Button(master)
