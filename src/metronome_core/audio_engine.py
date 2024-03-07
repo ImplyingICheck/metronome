7edbe813f36abd3226ddb27b2b7143ef3df8a4d2
Andrew <amhector@stanford.edu>
2024-03-06 18:06:33 -0800

    feat(AudioEngine): Implement create_rhythm_waveform
    Creates a sound waveform from a given rhythm

--- src/metronome_core/audio_engine.py
+++ src/metronome_core/audio_engine.py
@@ -1,9 +1,22 @@
 """Interface with sounddevice package. Creates a persistent output buffer to
 which data representing sound can be written."""
-from types import TracebackType
-
+from __future__ import annotations
 import numpy as np
 import sounddevice
+import dataclasses
+
+from collections.abc import Iterable
+from types import TracebackType
+from typing import TYPE_CHECKING, TypeAlias
+
+if TYPE_CHECKING:
+  Waveform: TypeAlias = np.ndarray[tuple[int], np.dtype[np.float32]]
+
+
+@dataclasses.dataclass
+class Wave:
+  frequency: int
+  duration: float
 
 
 class AudioEngine:
@@ -47,15 +60,26 @@ class AudioEngine:
   def close(self, ignore_errors: bool = True):
     self.output_stream.close(ignore_errors)
 
-  def _generate_sine_wave(self, frequency: float, duration: float):
+  def _generate_sine_wave(self, frequency: float, duration: float) -> Waveform:
     time_array = np.linspace(
         0, duration, int(self.sample_rate * duration), dtype="float32"
     )
     wave = np.sin(2 * np.pi * frequency * time_array)
     return wave
 
-  def _generate_silence(self, duration: float):
+  def _generate_silence(self, duration: float) -> Waveform:
     return np.linspace(0, 0, int(self.sample_rate * duration), dtype="float32")
+
+  def create_rhythm_waveform(self, rhythm: Iterable[Wave]) -> Waveform:
+    waveforms: list[Waveform] = []
+    for index, note in enumerate(rhythm):
+      if index % 2 == 0:
+        waveform = self._generate_sine_wave(note.frequency, note.duration)
+      else:
+        waveform = self._generate_silence(note.duration)
+      waveforms.append(waveform)
+    axis = 0 if self.channels == 1 else 1
+    return np.concatenate(waveforms, axis=axis)
 
   def play_sound(
       self, frequency: float, duration: float = 1, volume: float = 0.1
