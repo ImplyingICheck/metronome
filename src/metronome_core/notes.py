f9c170a5d6022576d9801904a624f5f55ef8b4ec
Andrew <amhector@stanford.edu>
2024-05-20 15:12:58 -0700

    feat(NoteSequence): Add to_waves class method
    Convert self.notes to a format understood by AudioEngine

--- src/metronome_core/notes.py
+++ src/metronome_core/notes.py
@@ -3,9 +3,15 @@ class to organize note groups."""
 from typing import TypeAlias
 from collections.abc import Iterable
 
+from metronome_core import audio_engine
 
 TimeSignature: TypeAlias = tuple[int, int]
 NoteValue: TypeAlias = float | int
+
+SECONDS_PER_MINUTE = 60
+CONCERT_PITCH = 440
+BEAT_DIVISOR = 1 / 4
+BEAT_DIVISOR_COMPLIMENT = 1 - BEAT_DIVISOR
 
 
 class Note:
@@ -27,6 +33,8 @@ class QuarterNote(Note):
 
 
 class NoteSequence:
+  """A fully defined series of sounds from which a metronome sound file can
+  be generated for the audio_engine.AudioEngine."""
 
   def __init__(
       self, tempo: int, notes: Iterable[Note], time_signature: TimeSignature
@@ -34,3 +42,16 @@ class NoteSequence:
     self.tempo = tempo
     self.notes = notes
     self.time_signature = time_signature
+
+  def to_waves(self) -> list[audio_engine.Wave]:
+    waves: list[audio_engine.Wave] = []
+    second_per_value = SECONDS_PER_MINUTE / self.tempo
+    for note in self.notes:
+      total_duration = note.value * second_per_value
+      accent = audio_engine.Wave(CONCERT_PITCH, total_duration * BEAT_DIVISOR)
+      waves.append(accent)
+      silence = audio_engine.Wave(
+          CONCERT_PITCH, total_duration * BEAT_DIVISOR_COMPLIMENT
+      )
+      waves.append(silence)
+    return waves
